85bc5aaca57f2c0754f2012161bdd0f1d608c3fa
Gabriel Jimenez <gabrielqueirozjimenez@gmail.com>
2024-01-03 23:08:33 -0300

    Conclusão Desafio Estacionamento

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,10 +1,54 @@
+using Microsoft.VisualBasic;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
+//--------------------------------------------------------------------------------------------------------VARIÁVEIS
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
         private List<string> veiculos = new List<string>();
+
+//--------------------------------------------------------------------------------------------------------CLIENTE
+        public void AdicionarVeiculo_Ciente()
+        {
+            Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculos.Add(Console.ReadLine());
+            Console.WriteLine($"Veículo adicionado com sucesso");
+            Thread.Sleep(2500);
+        }
+
+        public void RemoverVeiculo_Cliente(DateTime começo)
+        {
+            Console.WriteLine("Digite a placa do seu veículo para remover:");
+
+
+            string placa = Console.ReadLine();
+
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                DateTime fim = DateTime.Now;
+                
+
+                TimeSpan horas = fim - começo;
+                decimal taxa = 1.50m;
+                decimal valorTotal = 5m + (decimal)horas.TotalHours*taxa;
+
+                veiculos.Remove(placa);
+
+
+                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal:F2}");
+                Thread.Sleep(3000);
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Thread.Sleep(2500);
+            }
+        }
+
+
+//--------------------------------------------------------------------------------------------------------FUNCIONÁRIO
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
@@ -14,32 +58,25 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculos.Add(Console.ReadLine());
         }
 
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            string placa = Console.ReadLine();
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                int horas = int.Parse(Console.ReadLine());
+                decimal valorTotal = precoInicial + (precoPorHora*horas);
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                veiculos.Remove(placa);
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
@@ -55,8 +92,10 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                foreach(string automovel in veiculos)
+                {
+                    Console.WriteLine(automovel);
+                }
             }
             else
             {
--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -1,59 +1,151 @@
-﻿using DesafioFundamentos.Models;
+﻿using System.Collections;
+using System.Threading;
+using DesafioFundamentos.Models;
 
 // Coloca o encoding para UTF8 para exibir acentuação
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 decimal precoInicial = 0;
 decimal precoPorHora = 0;
+Estacionamento intro = new Estacionamento(0,0);
 
-Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
-                  "Digite o preço inicial:");
-precoInicial = Convert.ToDecimal(Console.ReadLine());
+string apresentaçao = "   BEM-VINDO AO ESTACIONAMENTO AUTÔNOMO";
+int linha_lenght = apresentaçao.Length+4;
 
-Console.WriteLine("Agora digite o preço por hora:");
-precoPorHora = Convert.ToDecimal(Console.ReadLine());
+//-----------------------------------------------------------------------------------------------APRESENTAÇÃO
+Console.WriteLine($"{new string('-',linha_lenght)}\n{apresentaçao}\n{new string('-',linha_lenght)}{new string('\n',2)}");
+Console.WriteLine("Como gostaria de prosseguir?\n");
+Console.WriteLine("1 - Cliente");
+Console.WriteLine("2 - Funcionário");
+string escolha = Console.ReadLine();
 
-// Instancia a classe Estacionamento, já com os valores obtidos anteriormente
-Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
-
-string opcao = string.Empty;
-bool exibirMenu = true;
-
-// Realiza o loop do menu
-while (exibirMenu)
+switch(escolha)
 {
-    Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
-
-    switch (Console.ReadLine())
+    case "1":
     {
-        case "1":
-            es.AdicionarVeiculo();
-            break;
+        Console.Clear();
+        Console.WriteLine("Qual o seu nome: ");
+        string nome = Console.ReadLine();
+        DateTime começo_cliente = DateTime.Now;
 
-        case "2":
-            es.RemoverVeiculo();
-            break;
+        while(true)
+        {
+            Console.Clear();
+            Console.WriteLine($"Olá {nome}.\nO preço padrão no estacionamento é de R$5,00 + R$1,50 por hora.");
+            Console.WriteLine($"O que você deseja fazer?{new string ('\n',2)}1 - Buscar vaga\n2 - Remover seu veículo\n3 - Cancelar");
+            string escolha_cliente = Console.ReadLine();
+            switch(escolha_cliente)
+            {
+                case "1":
+                {
+                    Console.WriteLine("Iremos agora verificar se há vagas disponiveis.");
 
-        case "3":
-            es.ListarVeiculos();
-            break;
+                    Thread.Sleep(3500);
+                    Console.Clear();
+                    Console.WriteLine("Verificando...");  
 
-        case "4":
-            exibirMenu = false;
-            break;
+                    Thread.Sleep(4000);
+                    Random random = new Random();
+                    int prob_vaga = random.Next(1,11);
 
-        default:
-            Console.WriteLine("Opção inválida");
-            break;
+                    if(prob_vaga<=3)
+                    {
+                        Console.WriteLine("Infelizmente, não há mais vagas disponiveis. =(");
+                        Thread.Sleep(2000);
+                        break;
+                    } else
+                    {
+                        Console.WriteLine("Vaga encontrada!!!");
+                        Thread.Sleep(3000);
+                        Console.Clear();
+                        Console.WriteLine("Vamos agora, iniciar o cadastro do seu veículo");
+                        Thread.Sleep(2000);
+                        intro.AdicionarVeiculo_Ciente();
+                    }
+                    break;
+                }
+                case "2":
+                {
+                    Console.WriteLine("Preparando o processo de remoção do veículo...");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                    intro.RemoverVeiculo_Cliente(começo_cliente);
+                    break;
+                }
+                case "3":
+                {
+                    Console.WriteLine("Encerrando processo...");
+                    Thread.Sleep(2500);
+                    Console.WriteLine("Tenha um bom dia");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    return;
+                }
+            }
+        }
     }
+//-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=------------------------------------------------------------
+    case "2":
+    {
+        Console.Clear();
+        Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                  "Digite o preço inicial:");
+        precoInicial = Convert.ToDecimal(Console.ReadLine());
 
-    Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
+        Console.WriteLine("Agora digite o preço por hora:");
+        precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+        // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+        Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+        string opcao = string.Empty;
+        bool exibirMenu = true;
+
+        // Realiza o loop do menu
+        while (exibirMenu)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a sua opção:");
+            Console.WriteLine("1 - Cadastrar veículo");
+            Console.WriteLine("2 - Remover veículo");
+            Console.WriteLine("3 - Listar veículos");
+            Console.WriteLine("4 - Encerrar");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    es.AdicionarVeiculo();
+                    break;
+
+                case "2":
+                    es.RemoverVeiculo();
+                    break;
+
+                case "3":
+                    es.ListarVeiculos();
+                    break;
+
+                case "4":
+                    exibirMenu = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+
+            Console.WriteLine("Pressione uma tecla para continuar");
+            Console.ReadLine();
+        }
+
+        Console.WriteLine("O programa se encerrou");
+        break;
+    }
+    
+    default:
+    {
+        Console.WriteLine("Opção invalida, tente outra vez");
+        break;
+    }
 }
-
-Console.WriteLine("O programa se encerrou");
+Console.WriteLine("Até Logo!!");
